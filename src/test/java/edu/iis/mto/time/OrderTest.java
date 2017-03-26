31f62e2b3a2158d1ce411acba6a291b225a69fd8
Piotr Dudek <194936@edu.p.lodz.pl>
2017-03-26 14:38:59 +0200

    Refaktoryzacja testów przez dodanie @Before

--- src/test/java/edu/iis/mto/time/OrderTest.java
+++ src/test/java/edu/iis/mto/time/OrderTest.java
@@ -1,5 +1,6 @@
 package edu.iis.mto.time;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -9,19 +10,25 @@ import static org.junit.Assert.*;
  */
 public class OrderTest {
 
+    public Order order;
+
+    @Before
+    public void setUp(){
+        Order order = new Order();
+        order.submit();
+    }
+
     @Test
     public void orderTestNotExpiredYet() throws Exception{
         AlternativeTimeProvider.timeToAdd = 24 * 60 * 60 * 1000;
-        Order order = new Order();
-        order.submit();
+
         order.confirm();
     }
 
     @Test(expected = OrderExpiredException.class)
     public void orderTestExpiredAlready() throws Exception{
         AlternativeTimeProvider.timeToAdd = 25 * 60 * 60 * 1000;
-        Order order = new Order();
-        order.submit();
+
         order.confirm();
     }
 }
